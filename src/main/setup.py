5749bddbf047488c59e2c6e8fc0949e80236dba3
Hugo Saporetti Junior <yorevs@gmail.com>
2020-05-29 12:15:01 -0300

    Refactorings and finish gradle.build

--- src/main/setup.py
+++ src/main/setup.py
@@ -1,12 +1,16 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md") as fh:
     long_description = fh.read()
+
+with open("VERSION") as fh:
+    version = fh.read()
 
 setuptools.setup(
     name='hhs-pylib',
-    version='0.9.0',
+    version=version,
     description='HomeSetup python3 library',
+    long_description=long_description,
     author='Hugo Saporetti Junior',
     author_email='yorevs@hotmail.com',
     packages = setuptools.find_packages(),
