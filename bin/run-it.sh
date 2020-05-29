ff55111a930e976fa809528084a565719c2ce4f3
Hugo Saporetti Junior <yorevs@gmail.com>
2020-05-29 11:16:06 -0300

    Package refactorings and gradle tasks

--- bin/run-it.sh
+++ bin/run-it.sh
@@ -5,6 +5,7 @@ CUR_DIR="$(pwd)"
 export PYTHONPATH="${CUR_DIR}/src/main"
 
 pushd "${CUR_DIR}/src/main" &> /dev/null || exit 1
+pyrcc5 resources.qrc -o resources_rc.py
 python3 main.py
 popd &> /dev/null || exit 1
 
