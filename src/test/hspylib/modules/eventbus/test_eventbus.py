2df4ebca6cb50dbbbcddd96ae4179fb517c22ce5
Hugo Saporetti Junior <yorevs@gmail.com>
2020-05-30 22:25:29 -0300

    Added eventbus tests

--- src/test/hspylib/modules/eventbus/test_eventbus.py
+++ src/test/hspylib/modules/eventbus/test_eventbus.py
@@ -8,6 +8,9 @@
 """
 
 import unittest
+from unittest.mock import MagicMock
+
+from main.hspylib.modules.eventbus.eventbus import EventBus
 
 
 class TestEventBus(unittest.TestCase):
@@ -18,8 +21,44 @@ class TestEventBus(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_case_1(self):
-        pass
+    def test_should_return_the_same_instance(self):
+        bus1 = EventBus.get('test-bus')
+        self.assertIsNotNone(bus1)
+        bus2 = EventBus.get('test-bus')
+        self.assertIsNotNone(bus2)
+        self.assertIs(bus1, bus2)
+
+    def test_should_not_return_the_same_instance(self):
+        bus1 = EventBus.get('test-bus1')
+        self.assertIsNotNone(bus1)
+        bus2 = EventBus.get('test-bus2')
+        self.assertIsNotNone(bus2)
+        self.assertIsNot(bus1, bus2)
+
+    def test_should_invoke_all_callbacks_from_subscribed_events(self):
+        bus1 = EventBus.get('test-bus1')
+        self.assertIsNotNone(bus1)
+        self.method1 = MagicMock()
+        self.method2 = MagicMock()
+        bus1.subscribe('test-event', self.method1)
+        bus1.subscribe('test-event', self.method2)
+        bus1.emit('test-event', age=41, name='hugo')
+        self.method1.assert_called_with({'age': 41, 'name': 'hugo'})
+        self.method2.assert_called_with({'age': 41, 'name': 'hugo'})
+        bus1.emit('test-event', age=25, name='jose')
+        self.method1.assert_called_with({'age': 25, 'name': 'jose'})
+        self.method2.assert_called_with({'age': 25, 'name': 'jose'})
+
+    def test_should_not_invoke_callbacks_from_unsubscribed_events(self):
+        bus1 = EventBus.get('test-bus1')
+        self.assertIsNotNone(bus1)
+        self.method1 = MagicMock()
+        self.method2 = MagicMock()
+        bus1.subscribe('test-event-Z', self.method1)
+        bus1.subscribe('test-event-Z', self.method2)
+        bus1.emit('test-event', age=41, name='hugo')
+        assert not self.method1.called
+        assert not self.method2.called
 
 
 if __name__ == '__main__':
