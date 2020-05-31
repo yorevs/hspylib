309119559275fad139c0e5a3ae8f0c0be7eb988e
Hugo Saporetti Junior <yorevs@gmail.com>
2020-05-31 19:03:35 -0300

    Add ServerMock

--- src/test/hspylib/modules/fetch/test_fetch.py
+++ src/test/hspylib/modules/fetch/test_fetch.py
@@ -1,13 +1,14 @@
 import sys
 import unittest
 
-from main.hspylib.modules.fetch.fetch import get
+from src.main.hspylib.modules.fetch.fetch import get
+from src.main.hspylib.modules.server_mock.server_mock import ServerMock
 
 
 class TestEventBus(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.server_mock = ServerMock()
 
     def tearDown(self):
         pass
