e73ed271898b9d870a7cbab248a7d076d7b2c59b
Hugo Saporetti Junior <yorevs@gmail.com>
2020-08-27 17:31:45 -0300

    Fixing logging problems and package moves

---
+++ src/test/hspylib/ui/cli/__init__.py
@@ -0,0 +1,7 @@
+# _*_ coding: utf-8 _*_
+#
+# Package: test.hspylib.ui.cli
+
+__all__ = [
+  "sample",
+]
--- src/test/hspylib/ui/cli/sample/static_menu.py
+++ src/test/hspylib/ui/cli/sample/static_menu.py
@@ -1,5 +1,5 @@
 from main.hspylib.ui.cli.menu_ui import MenuUi
-from main.hspylib.ui.cli.sample.main_menu import MainMenu
+from test.hspylib.ui.cli.sample.main_menu import MainMenu
 
 if __name__ == '__main__':
     mm = MenuUi(MainMenu())
