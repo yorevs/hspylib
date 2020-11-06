a624eb148776192882ae84ef141025fb4248b058
Hugo Saporetti Junior <yorevs@gmail.com>
2020-11-06 20:05:45 -0300

    adjustments

--- src/main/hspylib/core/tools/table_renderer.py
+++ src/main/hspylib/core/tools/table_renderer.py
@@ -97,19 +97,3 @@ class TableRenderer:
             else '| ' + '<empty>'.center(len(table_line) - 4, ' ') + ' |', file=file
         )
         print(table_line, file=file)
-
-
-if __name__ == '__main__':
-    h = [
-        'Col 1',
-        'Columns 2',
-        'Columns 3',
-        'Thats a big Column Header'
-    ]
-    data = [
-        ('One', 1, True, 2),
-        ('Two', 2, False, 3),
-        ('Three, four and five', 3, True, 3),
-    ]
-    tr = TableRenderer(h, data, 'TableRenderer example of usage')
-    tr.render()
