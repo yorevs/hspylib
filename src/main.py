ee2be33ddf633871e787d87e06eae7fafb19ab44
Hugo Saporetti Junior <yorevs@gmail.com>
2020-11-09 19:26:33 -0300

    adjustments

--- src/main.py
+++ src/main.py
@@ -3,27 +3,28 @@ import signal
 
 from hspylib.core.config.app_config import AppConfigs
 
-AppConfigs().logger().info(AppConfigs.INSTANCE)
-
-
-def exit_app(sig=None, frame=None):
-    print(frame or '', end='')
-    print('\033[2J\033[H')
-    print('Done.')
-    print('')
-    exit(sig)
-
 
 class Main:
     def __init__(self):
         self.configs = AppConfigs.INSTANCE
+        AppConfigs().logger().info(self.configs)
 
-    def run(self):
-        pass
+    @staticmethod
+    def run():
+        with open("welcome.txt") as fh:
+            print(fh.read(), end='')
+        with open("main/VERSION") as fh:
+            print("Version " + fh.read())
+
+    @staticmethod
+    def exit_app(sig=None, frame=None):
+        print(frame or '', end='')
+        print('\033[2J\033[H')
+        exit(sig)
 
 
 # Application entry point
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, exit_app)
+    signal.signal(signal.SIGINT, Main.exit_app)
     Main().run()
-    exit_app(0)
+    Main.exit_app(0)
