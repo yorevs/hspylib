ce0847751bc06327b13a5341dde6c5fbbe6adf76
Hugo Saporetti Junior <yorevs@gmail.com>
2020-11-20 20:14:03 -0300

    adjustments

--- src/main/hspylib/ui/cli/tools/validator/argument_validator.py
+++ src/main/hspylib/ui/cli/tools/validator/argument_validator.py
@@ -6,15 +6,11 @@ from hspylib.core.tools.validator import Validator
 class ArgumentValidator(Validator):
 
     def __call__(self, *args, **kwargs) -> Tuple[bool, List[dict]]:
-        errors = []
-        assert len(args) == 1, "Only one argument can be validated at a time"
-        assert isinstance(args[0], str), "Only string arguments can be validated"
-        assert self.validate_argument(args[0]), "Invalid arguments: {}".format(args[0])
-        super().__call__(args, kwargs)
-
-        return len(errors) == 0, errors
+        pass
 
     @staticmethod
-    def validate_argument(arguments, args_num: int) -> (bool, str):
-        return len(arguments) < args_num, "### Invalid number of arguments: {}, expecting: {}"\
+    def validate_argument(arguments, args_num: int) -> bool:
+        assert len(arguments) >= args_num, "Invalid number of arguments: {}, expecting: {}"\
             .format(len(arguments), args_num)
+
+        return True
