78478eab3efb80b38769b0e9df011b5c44f93215
Hugo Saporetti Junior <yorevs@gmail.com>
2021-03-05 02:50:28 -0300

    Add emojis

---
+++ src/main/hspylib/ui/cli/emojis/__init__.py
@@ -0,0 +1,10 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.9.14
+#
+# Package: main.hspylib.ui.cli.emojis
+
+__all__ = [
+  'emojis', 
+  'face_smiling'
+]
---
+++ src/main/hspylib/ui/cli/emojis/emojis.py
@@ -0,0 +1,28 @@
+import emoji
+
+from hspylib.core.enum.enumeration import Enumeration
+
+
+def emj_print(emoji_str: str) -> None:
+    print(Emoji.emojize(emoji_str), end='')
+
+
+class Emoji(Enumeration):
+    """
+        Emoji codes
+        Full list of emojis can be found here:
+          - https://unicode.org/emoji/charts/emoji-list.html
+    """
+    @classmethod
+    def emojize(cls, emoji_str: str) -> str:
+        return emoji.emojize(f"{emoji_str}")
+
+    @classmethod
+    def demojize(cls, emoji_str: str) -> str:
+        return emoji.demojize(f"{emoji_str}")
+
+    def __str__(self) -> str:
+        return self.value
+
+    def placeholder(self) -> str:
+        return f":{self.name}:"
