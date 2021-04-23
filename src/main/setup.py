a5c3c0d932fd55349551ba1a745a95df436150d7
Hugo Saporetti Junior <yorevs@gmail.com>
2021-04-23 11:04:43 -0300

    Improving the library installation for publishing

--- src/main/setup.py
+++ src/main/setup.py
@@ -1,33 +1,42 @@
+import pathlib
+
 import setuptools
 
-with open("README.md") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-with open(".version") as fh:
-    version = fh.read()
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
-with open("requirements.txt") as fh:
-    dependencies = list(filter(None, fh.read().splitlines()))
+# The version of the package
+VERSION = (HERE / ".version").read_text()
 
+# The package requirements
+REQUIREMENTS = list(filter(None, (HERE / "requirements.txt").read_text().splitlines()))
+
+# This call to setup() does all the work
 setuptools.setup(
     name='hspylib',
-    version=version,
-    description='HomeSetup python3 library',
+    version=VERSION,
+    description='HomeSetup python library',
     author='Hugo Saporetti Junior',
     author_email='yorevs@hotmail.com',
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/yorevs/hspylib",
-    long_description=long_description,
+    license='MIT',
     packages=setuptools.find_packages(),
     package_data={
         'hspylib.core.crud.db.sql': ['sql_stubs.sql']
     },
     include_package_data=True,
-    license='MIT',
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=dependencies,
+    install_requires=REQUIREMENTS,
 )
