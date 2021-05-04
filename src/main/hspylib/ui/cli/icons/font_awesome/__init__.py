e09ae1d01658c1d158a6d744e26c67c65bd38708
Hugo Saporetti Junior <yorevs@gmail.com>
2021-05-03 23:41:59 -0300

    ++version

--- src/main/hspylib/ui/cli/icons/font_awesome/__init__.py
+++ src/main/hspylib/ui/cli/icons/font_awesome/__init__.py
@@ -1,10 +1,11 @@
 # _*_ coding: utf-8 _*_
 #
-# HSPyLib v0.10.2
+# HSPyLib v0.10.3
 #
 # Package: main.hspylib.ui.cli.icons.font_awesome
 
 __all__ = [
   'awesome', 
+  'dashboard_icons', 
   'form_icons'
 ]
