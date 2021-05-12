a3b42e694fee72fc12b433da48b485bbd891ef32
Hugo Saporetti Junior <yorevs@gmail.com>
2021-05-11 21:10:17 -0300

    Add versioner app draft

---
+++ src/app/versioner/src/main/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.10.4
+#
+# Package: app.versioner.src.main
+"""Package initialization."""
+
+__all__ = [
+  '__main__', 
+  'core', 
+  'entity', 
+  'enum'
+]
