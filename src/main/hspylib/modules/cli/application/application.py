ab517cf784ce1ce23875d27b88ab1ce560d50bbc
Hugo Saporetti Junior <yorevs@gmail.com>
2021-05-12 19:45:48 -0300

    ++unittests for application

--- src/main/hspylib/modules/cli/application/application.py
+++ src/main/hspylib/modules/cli/application/application.py
@@ -17,14 +17,13 @@
 import getopt
 import logging as log
 import signal
-import sys
 from datetime import datetime
 from typing import Callable, List, Optional, Set, Tuple
 
 from hspylib.core.config.app_config import AppConfigs
 from hspylib.core.exception.exceptions import InvalidArgumentError, InvalidOptionError
 from hspylib.core.meta.singleton import Singleton
-from hspylib.core.tools.commons import syserr, sysout
+from hspylib.core.tools.commons import sysout
 from hspylib.modules.cli.application.argument import Argument
 from hspylib.modules.cli.application.argument_chain import ArgumentChain
 from hspylib.modules.cli.application.option import Option
@@ -32,7 +31,7 @@ from hspylib.modules.cli.application.option import Option
 
 class Application(metaclass=Singleton):
     """HSPyLib application framework"""
-    
+
     def __init__(
             self,
             app_name: str = None,
@@ -41,7 +40,7 @@ class Application(metaclass=Singleton):
             source_dir: str = None,
             resource_dir: str = None,
             log_dir: str = None):
-        
+
         signal.signal(signal.SIGINT, self.exit_handler)
         self.app_name = app_name
         self.app_version = app_version
@@ -59,7 +58,7 @@ class Application(metaclass=Singleton):
             self.with_option('h', 'help', handler=self.usage)
         if app_version:
             self.with_option('v', 'version', handler=self.version)
-    
+
     def run(self, *params, **kwargs) -> None:
         """Main entry point handler"""
         log.info('Run started {}'.format(datetime.now()))
@@ -67,13 +66,12 @@ class Application(metaclass=Singleton):
             self.setup_parameters(*params, **kwargs)
             self._parse_parameters(*params, **kwargs)
             self.main(*params, **kwargs)
-            self.exit_handler()
-        except (InvalidArgumentError, InvalidOptionError) as err:
-            syserr(str(err))
-            self.usage(1)
+        except (InvalidOptionError, InvalidArgumentError) as err:
+            self.usage(exit_code=1, no_exit=True)
+            raise err  # Re-Raise the exception so upper level layers can catch
         finally:
             log.info('Run finished {}'.format(datetime.now()))
-    
+
     def exit_handler(self, signum=0, frame=None, clear_screen: bool = False) -> None:
         """
         Handle interruptions to shutdown gracefully
@@ -90,42 +88,46 @@ class Application(metaclass=Singleton):
         self.cleanup()
         if clear_screen:
             sysout('%ED2%%HOM%')
-        sys.exit(exit_code)
-    
-    def usage(self, *args) -> None:
+        self.exit_handler(exit_code)
+
+    def usage(self, exit_code: int = 0, no_exit: bool = False) -> None:
         """Display the usage message and exit with the specified code ( or zero as default )
-        :param args: The application arguments
+        :param no_exit: Do no exit the application on usage call
+        :param exit_code: The exit code
         """
         sysout(self.app_usage)
-        self.exit_handler(args[0] if args and len(args) > 0 else 0)
-    
-    def version(self, *args) -> None:
+        if not no_exit:
+            self.exit_handler(exit_code)
+
+    def version(self, exit_code: int = 0, no_exit: bool = False) -> None:
         """Display the current program version and exit
-        :param args: The application arguments
+        :param no_exit: Do no exit the application on usage call
+        :param exit_code: The exit code
         """
         sysout('{} v{}'.format(self.app_name, '.'.join(map(str, self.app_version))))
-        self.exit_handler(args[0] if args and len(args) > 0 else 0)
-    
+        if not no_exit:
+            self.exit_handler(exit_code)
+
     def get_option(self, item: str) -> Optional[Option]:
         """Getter for application option"""
         return next((op for key, op in self.options.items() if op.is_eq(item)), None)
-    
+
     def get_argument(self, index: int) -> Optional[Argument]:
         """Getter for application arguments"""
         return self.args[index] if self.args and 0 <= index < len(self.args) else None
-    
+
     def setup_parameters(self, *params, **kwargs) -> None:  # pylint: disable=unused-argument,no-self-use
         """Initialize application parameters and options"""
         log.info('setup_parameters was not overridden')
-    
+
     def main(self, *params, **kwargs) -> None:  # pylint: disable=unused-argument,no-self-use
         """Execute the application's main statements"""
         log.info('main was not overridden')
-    
+
     def cleanup(self) -> None:  # pylint: disable=no-self-use
         """Execute code cleanup before exiting"""
         log.info('cleanup was not overridden')
-    
+
     def with_option(
             self,
             shortopt: chr,
@@ -134,20 +136,20 @@ class Application(metaclass=Singleton):
             handler: Callable = None) -> None:
         """Specify an option for the command line"""
         self.options[longopt] = Option(shortopt, longopt, has_argument, handler)
-    
+
     def with_arguments(
             self,
             chained_args: Set[ArgumentChain.ChainedArgument]) -> None:
         """Specify an argument for the command line"""
         self.cond_args_chain = chained_args
-    
+
     def _parse_parameters(self, parameters: List[str]) -> None:
         """ Handle program parameters.
         :param parameters: The list of unparsed program parameters passed by the command line
         """
         # First parse all options, then, parse arguments
         self._parse_arguments(self._parse_options(parameters))
-    
+
     def _parse_options(self, parameters: List[str]) -> List[str]:
         """Handle program options in the form: Short opts: -<C>, Long opts: --<Word>
         :param parameters: The list of unparsed program parameters passed by the command line
@@ -164,7 +166,7 @@ class Application(metaclass=Singleton):
             return args
         except getopt.GetoptError as err:
             raise InvalidOptionError(f"### Invalid option: {str(err)}") from err
-    
+
     def _parse_arguments(self, provided_args: List[str]) -> None:
         """Handle program arguments
         :param provided_args: Arguments left after processing all options
@@ -188,7 +190,7 @@ class Application(metaclass=Singleton):
                     continue  # Just try the next chain
             if not valid:
                 raise InvalidArgumentError(f"Invalid arguments  \"{', '.join(provided_args)}\"")
-    
+
     def _recursive_set(self, idx: int, argument: Argument, provided_args: List[str]) -> None:
         """ Try to set a value for each provided argument. If any failure setting occur, raise an exception to be caught
             by the parse function, so they can try the next chain in the loop.
@@ -199,7 +201,7 @@ class Application(metaclass=Singleton):
             raise LookupError(
                 f'Invalid argument "{provided_args[idx]}"')
         self._recursive_set(idx + 1, argument.next_in_chain, provided_args)
-    
+
     @staticmethod
     def _validate_args_in_chain(arg: Argument) -> bool:
         """Validate all arguments following the chain path"""
@@ -209,9 +211,9 @@ class Application(metaclass=Singleton):
             missing += 1 if not next_arg.value and next_arg.required else 0
             next_arg = next_arg.next_in_chain
         return missing == 0
-    
+
     def _shortopts(self) -> str:
         return ''.join([op.shortopt.replace('-', '') for key, op in self.options.items()])
-    
+
     def _longopts(self) -> List[str]:
         return [op.longopt.replace('-', '') + ' ' for key, op in self.options.items()]
