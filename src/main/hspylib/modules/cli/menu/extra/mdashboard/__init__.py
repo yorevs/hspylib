347fc77d39ee4fd0ec7ea38b228f23148aedc149
Hugo Saporetti Junior <yorevs@gmail.com>
2021-05-20 02:33:52 -0300

    Removing inner classes from dashboard

---
+++ src/main/hspylib/modules/cli/menu/extra/mdashboard/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.10.5
+#
+# Package: main.hspylib.modules.cli.menu.extra.mdashboard
+"""Package initialization."""
+
+__all__ = [
+  'dashboard_builder', 
+  'dashboard_item', 
+  'dashboard_item_builder', 
+  'mdashboard'
+]
