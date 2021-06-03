ffd801fe81a0f9c890b87921d59a37aeda1e38d5
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-02 23:23:57 -0300

    Updated publication docs

--- src/main/hspylib/core/tools/git_tools.py
+++ src/main/hspylib/core/tools/git_tools.py
@@ -27,23 +27,23 @@ class GitTools:
     @staticmethod
     def changelog(from_tag: str, to_tag: str) -> str:
         """TODO"""
-        return Terminal.shell_exec(f"git log --oneline --pretty=format:%h %ad %s --date=short {from_tag}^..{to_tag}^")
+        return Terminal.shell_exec(f"git log --oneline --pretty='format:%h %ad %s' --date=short {from_tag}^..{to_tag}^")
 
     @staticmethod
     def unreleased() -> str:
         """TODO"""
         latest_tag = Terminal.shell_exec('git describe --tags --abbrev=0 HEAD^')
-        return Terminal.shell_exec(f"git log --oneline --pretty=format:%h %ad %s --date=short {latest_tag}..HEAD")
+        return Terminal.shell_exec(f"git log --oneline --pretty='format:%h %ad %s' --date=short '{latest_tag}'..HEAD")
 
     @staticmethod
     def release_date(tag_name: str) -> str:
         """TODO"""
-        return Terminal.shell_exec(f"git log -1 --format=%ad --date=short {tag_name}")
+        return Terminal.shell_exec(f"git log -1 --pretty='format:%ad' --date=short {tag_name}")
 
     @staticmethod
     def tag_list() -> str:
         """TODO"""
-        return Terminal.shell_exec('git log --tags --simplify-by-decoration --pretty="format:%ci %d"')
+        return Terminal.shell_exec("git log --tags --simplify-by-decoration --pretty='format:%ci %d'")
 
     @staticmethod
     def create_tag(version: str, commit_id: str = 'HEAD', description: str = None) -> str:
@@ -58,3 +58,6 @@ class GitTools:
     @staticmethod
     def show_file(filename: str, commit_id: str = 'HEAD') -> str:
         return Terminal.shell_exec(f"git show {commit_id}:{filename}")
+
+if __name__ == '__main__':
+    print(GitTools.unreleased())
