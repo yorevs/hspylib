22f8fe46c13ee540996f236f0e9a94fd303c1942
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-04 23:53:18 -0300

    Updated kafka docker compose and examples

---
+++ devel/kafka-prod.py
@@ -0,0 +1,26 @@
+from cffi.backend_ctypes import xrange
+from confluent_kafka import Producer
+
+settings = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: {0}: {1}"
+              .format(msg.value(), err.str()))
+    else:
+        print("Message produced: {0}".format(msg.value()))
+
+if __name__ == '__main__':
+    p = Producer(settings)
+    try:
+        for val in xrange(1, 1000):
+            p.produce('foobar', 'myvalue #{0}'.format(val), callback=acked)
+            p.poll(0.5)
+
+    except KeyboardInterrupt:
+        pass
+
+    p.flush(30)
+
