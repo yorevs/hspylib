e2e825da69cbfe9885f834fb90183049dfd8bc43
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-11 16:50:22 -0300

    Upgrade on qt components and Kafman

--- src/app/kafman/src/main/core/kafman_consumer.py
+++ src/app/kafman/src/main/core/kafman_consumer.py
@@ -8,8 +8,16 @@ from hspylib.core.metaclass.singleton import Singleton
 class KafmanConsumer(metaclass=Singleton):
     """TODO"""
 
-    def __init__(self, settings: dict, topic: List[str]):
+    def __init__(self):
         super().__init__()
-        self.settings = settings
-        self.topic = topic
-        self.consumer = Consumer(settings)
+        self.topic = None
+        self.consumer = None
+
+    def start(self, settings: dict):
+        if not self.consumer:
+            self.consumer = Consumer(settings)
+
+    def stop(self):
+        if self.consumer:
+            del self.consumer
+            self.consumer = None
--- src/main/hspylib/modules/qt/promotions/hlabel.py
+++ src/main/hspylib/modules/qt/promotions/hlabel.py
@@ -5,7 +5,7 @@
    TODO Purpose of the file
    @project: HSPyLib
    hspylib.main.hspylib.modules.qt.promotions
-      @file: panel.py
+      @file: hlabel.py
    @created: Tue, 4 May 2021
     @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
       @site: https://github.com/yorevs/hspylib
@@ -15,17 +15,16 @@
 """
 
 from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QLabel
 
 
-class Panel(QFrame):
+class HLabel(QLabel):
     """TODO"""
 
-    keyPressed = pyqtSignal(int)
+    clicked = pyqtSignal(int)
 
     def __init__(self, parent):
         super().__init__(parent=parent)
 
-    def keyPressEvent(self, event) -> None:
-        """TODO"""
-        self.keyPressed.emit(event.key())
+    def mousePressEvent(self, ev) -> None:  # pylint: disable=unused-argument
+        self.clicked.emit()
