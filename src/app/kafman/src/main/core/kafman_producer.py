c7fb3c18df34feea17c8686f4060e40e7d3d7e71
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-11 16:50:22 -0300

    Upgrade on qt components and Kafman

--- src/app/kafman/src/main/core/kafman_producer.py
+++ src/app/kafman/src/main/core/kafman_producer.py
@@ -8,8 +8,26 @@ from hspylib.core.metaclass.singleton import Singleton
 class KafmanProducer(metaclass=Singleton):
     """TODO"""
 
-    def __init__(self, settings: dict, topic: List[str]):
+    def __init__(self):
         super().__init__()
-        self.settings = settings
-        self.topic = topic
-        self.producer = Producer(settings)
+        self.topic = None
+        self.producer = None
+
+    def flush(self, timeout: int = 0):
+        if self.producer:
+            self.producer.flush(timeout=timeout)
+
+    def purge(self):
+        if self.producer:
+            self.producer.purge()
+
+    def start(self, settings: dict):
+        if not self.producer:
+            self.producer = Producer(settings)
+
+    def stop(self):
+        if self.producer:
+            self.purge()
+            self.flush()
+            del self.producer
+            self.producer = None
--- src/app/kafman/src/main/views/main_qt_view.py
+++ src/app/kafman/src/main/views/main_qt_view.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import QDialog
 
 from hspylib.core.config.app_config import AppConfigs
 from hspylib.modules.qt.views.qt_view import QtView
@@ -15,12 +15,14 @@ class MainQtView(QtView):
     """
     UI_FILE = 'main_qt_view.ui'
 
+    REQUIRED_SETTINGS = ['bootstrap.servers']
+
     def __init__(self):
         # Must come after the initialization above
         super().__init__(self.UI_FILE)
         self.configs = AppConfigs.INSTANCE
-        self.consumer = None
-        self.producer = None
+        self._consumer = KafmanConsumer()
+        self._producer = KafmanProducer()
         self._all_settings = {
             'producer': {
                 'bootstrap.servers': 'localhost:9092',
@@ -45,20 +47,24 @@ class MainQtView(QtView):
         self.ui.tool_box.setCurrentIndex(0)
         self.ui.splitter_pane.setSizes([300, 824])
         self.ui.tab_widget.currentChanged.connect(self._set_current_tab)
+        self.ui.lbl_status_text.setTextFormat(Qt.RichText)
         self.ui.btn_start.clicked.connect(self._start)
         self.ui.btn_stop.clicked.connect(self._stop)
-        self.ui.tbtn_prod_settings_add.clicked.connect(self._add_prod_setting)
-        self.ui.tbtn_cons_settings_add.clicked.connect(self._add_cons_setting)
-        self.ui.lbl_status_text.setTextFormat(Qt.RichText)
-        self.ui.lst_prod_settings.currentRowChanged.connect(self._producer_setting)
-        self.ui.lst_cons_settings.currentRowChanged.connect(self._consumer_setting)
         self.ui.cmb_topic.lineEdit().setPlaceholderText("Select or type comma (,) separated topics")
-        self.ui.le_prod_settings.editingFinished.connect(self._edit_producer_setting)
-        self.ui.le_cons_settings.editingFinished.connect(self._edit_consumer_setting)
-        for lst in [self.ui.lst_prod_settings, self.ui.lst_cons_settings]:
-            for index in range(lst.count()):
-                item = lst.item(index)
-                item.setFlags(item.flags() | Qt.ItemIsEditable)
+        # Producer controls
+        self.ui.tbtn_prod_settings_add.clicked.connect(lambda: self.ui.lst_prod_settings.set_item('new.setting'))
+        self.ui.tbtn_prod_settings_del.clicked.connect(self._del_item)
+        self.ui.lst_prod_settings.currentRowChanged.connect(self._get_setting)
+        self.ui.lst_prod_settings.set_editable()
+        self.ui.le_prod_settings.editingFinished.connect(self._edit_setting)
+        self.ui.lst_prod_settings.itemChanged.connect(self._edit_setting)
+        # Consumer controls
+        self.ui.tbtn_cons_settings_add.clicked.connect(lambda: self.ui.lst_cons_settings.set_item('new.setting'))
+        self.ui.tbtn_cons_settings_del.clicked.connect(self._del_item)
+        self.ui.lst_cons_settings.currentRowChanged.connect(self._get_setting)
+        self.ui.lst_cons_settings.set_editable()
+        self.ui.le_cons_settings.editingFinished.connect(self._edit_setting)
+        self.ui.lst_cons_settings.itemChanged.connect(self._edit_setting)
 
     def _topics(self) -> List[str]:
         """TODO"""
@@ -66,6 +72,7 @@ class MainQtView(QtView):
         return current_text.split(',') if current_text else []
 
     def _message(self) -> str:
+        """TODO"""
         return self.ui.txt_producer.toPlainText()
 
     def _settings(self) -> dict:
@@ -85,65 +92,72 @@ class MainQtView(QtView):
         self.ui.stk_strategy.setCurrentIndex(index)
         self.ui.stk_statistics.setCurrentIndex(index)
 
-    def _add_prod_setting(self):
-        lst = self.ui.lst_prod_settings
-        if not lst.findItems('new.setting', Qt.MatchFixedString):
-            item = QListWidgetItem('new.setting')
-            item.setFlags(int(item.flags()) | Qt.ItemIsEditable)
-            lst.addItem(item)
-
-    def _producer_setting(self) -> None:
+    def _get_setting(self) -> None:
         """TODO"""
-        setting = self.ui.lst_prod_settings.currentItem().text()
-        try:
-            self.ui.le_prod_settings.setText(self._all_settings['producer'][setting])
-        except KeyError:
-            self.ui.le_prod_settings.setText('')
-            self._all_settings['producer'][setting] = ''
-
-    def _edit_producer_setting(self) -> None:
-        item = self.ui.lst_prod_settings.currentItem()
+        lst = self.ui.lst_prod_settings if self._is_producer() else self.ui.lst_cons_settings
+        edt = self.ui.le_prod_settings if self._is_producer() else self.ui.le_cons_settings
+        ktype = self.ui.btn_start.text().lower() + 'r'
+        item = lst.currentItem()
         if item and item.text():
             setting = item.text()
-            value = self.ui.le_prod_settings.text()
-            self._all_settings['producer'][setting] = value
-            self._display_text(f"Producer setting {setting} saved")
+            if setting in self._all_settings[ktype]:
+                edt.setText(self._all_settings[ktype][setting])
+            else:
+                edt.setText('')
+                self._all_settings[ktype][setting] = ''
 
-    def _add_cons_setting(self):
-        lst = self.ui.lst_cons_settings
-        if not lst.findItems('new.setting', Qt.MatchFixedString):
-            item = QListWidgetItem('new.setting')
-            item.setFlags(int(item.flags()) | Qt.ItemIsEditable)
-            lst.addItem(item)
-
-    def _consumer_setting(self) -> None:
+    def _edit_setting(self) -> None:
         """TODO"""
-        setting = self.ui.lst_cons_settings.currentItem().text()
-        try:
-            self.ui.le_cons_settings.setText(self._all_settings['consumer'][setting])
-        except KeyError:
-            self.ui.le_cons_settings.setText('')
-            self._all_settings['consumer'][setting] = ''
-
-    def _edit_consumer_setting(self) -> None:
-        item = self.ui.lst_cons_settings.currentItem()
-        if item and item.text():
+        lst = self.ui.lst_prod_settings if self._is_producer() else self.ui.lst_cons_settings
+        edt = self.ui.le_prod_settings if self._is_producer() else self.ui.le_cons_settings
+        ktype = self.ui.btn_start.text().lower() + 'r'
+        item = lst.currentItem()
+        if item:
             setting = item.text()
-            value = self.ui.le_cons_settings.text()
-            self._all_settings['consumer'][setting] = value
-            self._display_text(f"Consumer setting {setting} saved")
+            old_setting = next((s for i, s in enumerate(self._all_settings[ktype]) if i == lst.currentRow()), '')
+            if setting:
+                if setting not in self._all_settings[ktype]:
+                    del self._all_settings[ktype][old_setting]
+                if edt.text():
+                    self._all_settings[ktype][setting] = edt.text()
+                    self._display_text(f"{ktype.capitalize()} setting {setting} saved")
+                else:
+                    edt.setText(self._all_settings[ktype][setting])
+            else:
+                setting = old_setting
+                edt.setText(self._all_settings[ktype][setting])
+                item.setText(setting)
+                if setting:
+                    self._display_error(f"Setting {setting} is required")
+
+    def _del_item(self) -> None:
+        """TODO"""
+        lst = self.ui.lst_prod_settings if self._is_producer() else self.ui.lst_cons_settings
+        ktype = self.ui.btn_start.text().lower() + 'r'
+        item = lst.currentItem()
+        if item:
+            setting = item.text()
+            if setting and setting not in self.REQUIRED_SETTINGS:
+                lst.del_item(lst.currentRow())
+                del self._all_settings[ktype][setting]
+            else:
+                self._display_error(f"Setting {setting} is required")
 
     def _start(self) -> None:
         """TODO"""
         settings = self._settings()
         topics = self._topics()
-        if topics:
+        if topics and all(s in settings for s in self.REQUIRED_SETTINGS):
             self.ui.cmb_topic.addItem(','.join(topics))
             if self._is_producer():
-                self.producer = KafmanProducer(settings, topics)
-                self._display_text(f"Started producing to topics {topics}", '#00FF00')
+                if self._message():
+                    self._producer.start(settings)
+                    self._display_text(f"Started producing to topics {topics}", '#00FF00')
+                else:
+                    self._display_error('No message defined')
+                    return
             else:
-                self.consumer = KafmanConsumer(settings, topics)
+                self._consumer.start(settings)
                 self._display_text(f"Started consuming from topics {topics}", '#00FF00')
             self.ui.btn_stop.setEnabled(True)
             self.ui.btn_start.setEnabled(False)
@@ -155,8 +169,10 @@ class MainQtView(QtView):
     def _stop(self):
         """TODO"""
         if self._is_producer():
+            self._producer.stop()
             self._display_text(f"Production to topic {self._topics()} stopped", '#FFFF00')
         else:
+            self._consumer.stop()
             self._display_text(f"Consumption from topic {self._topics()} stopped", '#FFFF00')
         self.ui.btn_stop.setEnabled(False)
         self.ui.btn_start.setEnabled(True)
