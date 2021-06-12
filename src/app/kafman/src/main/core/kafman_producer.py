a227f60ac91e7958c696cc2f8fdefe79eb0d3cd4
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-11 22:19:52 -0300

    First working version

--- src/app/kafman/src/main/core/kafman_producer.py
+++ src/app/kafman/src/main/core/kafman_producer.py
@@ -8,26 +8,54 @@ from hspylib.core.metaclass.singleton import Singleton
 class KafmanProducer(metaclass=Singleton):
     """TODO"""
 
+    @staticmethod
+    def message_produced(err, msg) -> None:
+        """TODO"""
+        if err is not None:
+            print(f"Failed to deliver message: {msg.value()}: {err.str()}")
+        else:
+            print(f"Message produced: {msg.value()}")
+
     def __init__(self):
         super().__init__()
         self.topic = None
         self.producer = None
+        self.started = False
 
-    def flush(self, timeout: int = 0):
+    def flush(self, timeout: int = 0) -> None:
+        """TODO"""
         if self.producer:
             self.producer.flush(timeout=timeout)
 
-    def purge(self):
+    def purge(self) -> None:
+        """TODO"""
         if self.producer:
             self.producer.purge()
 
-    def start(self, settings: dict):
+    def start(self, settings: dict) -> None:
+        """TODO"""
         if not self.producer:
             self.producer = Producer(settings)
+            self.started = True
 
-    def stop(self):
+    def stop(self) -> None:
+        """TODO"""
         if self.producer:
             self.purge()
             self.flush()
             del self.producer
             self.producer = None
+            self.started = False
+
+    def produce(self, topics: List[str], messages: List[str]) -> None:
+        """TODO"""
+        if self.producer is not None:
+            try:
+                for topic in topics:
+                    for msg in messages:
+                        self.producer.produce(topic, msg, callback=self.message_produced)
+                        self.producer.poll(0.5)
+            except KeyboardInterrupt:
+                pass
+            finally:
+                self.producer.flush(30)
