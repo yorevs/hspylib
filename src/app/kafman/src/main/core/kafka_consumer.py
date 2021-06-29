26afd30f7397d197ca164e8e7f9cb56d31e08f38
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-28 22:14:52 -0300

    Fixups

--- src/app/kafman/src/main/core/kafka_consumer.py
+++ src/app/kafman/src/main/core/kafka_consumer.py
@@ -3,67 +3,60 @@ from typing import List
 
 from PyQt5.QtCore import pyqtSignal, QObject
 from confluent_kafka.cimpl import Consumer
-from confluent_kafka.error import ConsumeError
 
-from hspylib.core.enums.charset import Charset
 from hspylib.core.tools.commons import syserr
-from kafman.src.main.core.constants import PARTITION_EOF
 
-
+# Example at https://docs.confluent.io/platform/current/tutorials/examples/clients/docs/python.html
 class KafkaConsumer(QObject):
     """TODO"""
 
-    messageConsumed = pyqtSignal(str, str)
+    messageConsumed = pyqtSignal(str, int, int, bytes)
 
     def __init__(self, poll_interval: float = 0.5):
         super().__init__()
-        self.started = False
-        self.poll_interval = poll_interval
-        self.topic = None
-        self.consumer = None
-        self.tr = None
+        self._started = False
+        self._poll_interval = poll_interval
+        self._consumer = None
+        self._worker_thread = None
 
     def start(self, settings: dict) -> None:
         """TODO"""
-        if self.consumer is None:
-            self.consumer = Consumer(settings)
-            self.started = True
+        if self._consumer is None:
+            self._consumer = Consumer(settings)
+            self._started = True
 
     def stop(self) -> None:
         """TODO"""
-        if self.consumer is not None:
-            self.started = False
+        if self._consumer is not None:
+            self._started = False
 
     def consume(self, topics: List[str]) -> None:
         """TODO"""
-        if self.started:
-            self.tr = threading.Thread(target=self._consume, args=(topics,))
-            self.tr.name = 'kafka-consumer'
-            self.tr.setDaemon(True)
-            self.tr.start()
+        if self._started:
+            self._worker_thread = threading.Thread(target=self._consume, args=(topics,))
+            self._worker_thread.name = 'kafka-consumer'
+            self._worker_thread.setDaemon(True)
+            self._worker_thread.start()
 
     def _consume(self, topics: List[str]) -> None:
         """TODO"""
         try:
-            self.consumer.subscribe(topics)
-            while self.started and self.consumer is not None:
-                message = self.consumer.poll(self.poll_interval)
+            self._consumer.subscribe(topics)
+            while self._started and self._consumer is not None:
+                message = self._consumer.poll(self._poll_interval)
                 if message is None:
                     continue
-                elif not message.error():
-                    msg = message.value().decode(Charset.UTF_8.value)
-                    self.messageConsumed.emit(message.topic(), msg)
-                elif message.error().code() == PARTITION_EOF:
-                    syserr(f"End of partition reached {message.topic()}/{message.partition()}")
+                elif message.error():
+                    syserr(f"An error occurred consuming from Kafka: {message.error().str()}")
                 else:
-                    syserr(f"Error occurred: {message.error().str()}")
+                    self.messageConsumed.emit(
+                        message.topic(), message.partition(), message.offset(), message.value()
+                    )
         except KeyboardInterrupt:
             syserr("Keyboard interrupted")
-        except ConsumeError as err:
-            syserr(f"An error occurred: {str(err)}")
         finally:
-            if self.consumer:
-                self.consumer.close()
-                del self.consumer
-                self.consumer = None
-                self.tr = None
+            if self._consumer:
+                self._consumer.close()
+                del self._consumer
+                self._consumer = None
+                self._worker_thread = None
