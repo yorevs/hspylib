005265e357665bc2f57243b9b2391340e2d2e756
Hugo Saporetti Junior <yorevs@gmail.com>
2021-06-30 19:20:45 -0300

    Add kafka package and some improvements

--- src/main/hspylib/modules/qt/stream_capturer.py
+++ src/main/hspylib/modules/qt/stream_capturer.py
@@ -19,7 +19,7 @@ from time import sleep
 
 from PyQt5.QtCore import QThread, pyqtSignal
 
-from hspylib.core.tools.commons import is_debugging
+from hspylib.core.tools.commons import is_debugging, syserr
 
 
 class StreamCapturer(QThread):
@@ -30,14 +30,16 @@ class StreamCapturer(QThread):
 
     class StdoutWorker(QThread):
         """QThread worker to capture stdout messages"""
+
         streamCaptured = pyqtSignal(str)
+
         def __init__(self, parent, poll_interval: float):
             super().__init__()
             self._poll_interval = poll_interval
             self._parent = parent
 
         def run(self):
-            self.setObjectName('stdout-worker')
+            self.setObjectName(f"stdout-worker-{hash(self)}")
             with io.StringIO() as buf, redirect_stdout(buf):
                 while not self._parent.isFinished():
                     output = buf.getvalue()
@@ -48,7 +50,9 @@ class StreamCapturer(QThread):
 
     class StderrWorker(QThread):
         """QThread worker to capture stderr messages"""
+
         streamCaptured = pyqtSignal(str)
+
         def __init__(self, parent, poll_interval: float):
             super().__init__()
             self._poll_interval = poll_interval
@@ -64,21 +68,39 @@ class StreamCapturer(QThread):
                         buf.truncate(0)
                     sleep(self._poll_interval)
 
-    def __init__(self, stdout_poll_interval: float = 0.5, stderr_poll_interval: float = 0.5):
+    def __init__(
+            self,
+            capture_stderr: bool = True,
+            capture_stdout: bool = True,
+            stdout_poll_interval: float = 0.5,
+            stderr_poll_interval: float = 0.5):
+
+        assert capture_stderr or capture_stdout, 'At least one capturer must be started'
         super().__init__()
         self.setObjectName('stream-capturer')
-        self._stderr_capturer = self.StderrWorker(self, stderr_poll_interval)
-        self._stdout_capturer = self.StdoutWorker(self, stdout_poll_interval)
-        self._stderr_capturer.streamCaptured.connect(lambda msg: self.stderrCaptured.emit(msg))
-        self._stdout_capturer.streamCaptured.connect(lambda msg: self.stdoutCaptured.emit(msg))
+        self._capture_stdout = capture_stdout
+        self._capture_stderr =capture_stderr
         self._poll_interval = stdout_poll_interval + stderr_poll_interval
 
+        if self._capture_stderr:
+            self._stderr_capturer = self.StderrWorker(self, stderr_poll_interval)
+            self._stderr_capturer.streamCaptured.connect(lambda msg: self.stderrCaptured.emit(msg))
+        if self._capture_stdout:
+            self._stdout_capturer = self.StdoutWorker(self, stdout_poll_interval)
+            self._stdout_capturer.streamCaptured.connect(lambda msg: self.stdoutCaptured.emit(msg))
+
     def run(self) -> None:
-        self._stderr_capturer.start()
-        self._stdout_capturer.start()
+
+        if self._capture_stderr:
+            self._stderr_capturer.start()
+        if self._capture_stdout:
+            self._stdout_capturer.start()
+
         while not self.isFinished():
             sleep(self._poll_interval)
 
     def start(self, priority: QThread.Priority = QThread.NormalPriority) -> None:
         if not is_debugging():
             super().start(priority)
+        else:
+            syserr('Stderr/Stdout capture is not started in debugging mode')
