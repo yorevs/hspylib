5a55c928a7e4b1d3ae24b9ed677a2928b98b5332
Hugo Saporetti Junior <yorevs@gmail.com>
2022-02-10 17:20:31 -0300

    Moving apps into modules directory to modularize the library

---
+++ modules/versioner/src/main/enums/part.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   TODO Purpose of the file
+   @project: HSPyLib
+   hspylib.app.versioner.src.main.enums
+      @file: part.py
+   @created: Tue, 11 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2021, HSPyLib team
+"""
+
+from hspylib.core.enums import Enumeration
+
+
+class Part(Enumeration):
+    PATCH = 1
+    MINOR = 2
+    MAJOR = 4
+
+    def __str__(self):
+        return str(self.name).lower()
+
+    def __repr__(self):
+        return str(self)
