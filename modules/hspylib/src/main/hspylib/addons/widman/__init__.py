774b966c80162efa0983ce765eaf3bb57eb7b547
Hugo Saporetti Junior <yorevs@gmail.com>
2022-02-22 16:06:17 -0300

    ++version published

--- modules/hspylib/src/main/hspylib/addons/widman/__init__.py
+++ modules/hspylib/src/main/hspylib/addons/widman/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v0.11.21
+# hspylib v0.11.22
 #
 # Package: main.hspylib.addons.widman
 """Package initialization."""
@@ -11,4 +11,4 @@ __all__ = [
   'widgets', 
   'widman'
 ]
-__version__ = '0.11.21'
+__version__ = '0.11.22'
