aefd74ab7906c1852b5ba0615c724437029d1386
Hugo Saporetti Junior <yorevs@gmail.com>
2022-02-22 23:51:54 -0300

    Fix hspylib applications using wrong resource_dir

--- modules/hspylib/src/main/hspylib/__init__.py
+++ modules/hspylib/src/main/hspylib/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v0.11.25
+# hspylib v0.11.28
 #
 # Package: main.hspylib
 """Package initialization."""
@@ -11,4 +11,4 @@ __all__ = [
   'core', 
   'modules'
 ]
-__version__ = '0.11.25'
+__version__ = '0.11.28'
