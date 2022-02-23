b066048c696c6b3fe2dac113561a94f6b50108f8
Hugo Saporetti Junior <yorevs@gmail.com>
2022-02-23 04:23:47 -0300

    Refinements and fix vault get by name

--- modules/vault/src/main/vault/__init__.py
+++ modules/vault/src/main/vault/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-vault v0.9.17
+# hspylib-vault v0.9.18
 #
 # Package: main.vault
 """Package initialization."""
@@ -11,4 +11,4 @@ __all__ = [
   'entity', 
   'exception'
 ]
-__version__ = '0.9.17'
+__version__ = '0.9.18'
