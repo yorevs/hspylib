370904126c5b4f1cf1898673ebd4b448ea3e4a6c
Hugo Saporetti Junior <yorevs@gmail.com>
2022-06-03 00:34:12 -0300

    After publish syncs

--- modules/hspylib/src/demo/calculator/ui/qt/__init__.py
+++ modules/hspylib/src/demo/calculator/ui/qt/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v0.11.59
+# hspylib v0.11.60
 #
 # Package: demo.calculator.ui.qt
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
   'views'
 ]
-__version__ = '0.11.59'
+__version__ = '0.11.60'
