aa8bfb26e85ff53dbf2dc811da8e3243cc3b2a61
Hugo Saporetti Junior <yorevs@gmail.com>
2022-06-03 18:03:52 -0300

    Fixed and improved the config properties

--- modules/hspylib/src/main/hspylib/modules/__init__.py
+++ modules/hspylib/src/main/hspylib/modules/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v0.11.60
+# hspylib v0.11.61
 #
 # Package: main.hspylib.modules
 """Package initialization."""
@@ -15,4 +15,4 @@ __all__ = [
   'security', 
   'web'
 ]
-__version__ = '0.11.60'
+__version__ = '0.11.61'
