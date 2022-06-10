42e09e271a43d13b0f436a823ceb647b3e22fc24
Hugo Saporetti Junior <yorevs@gmail.com>
2022-06-09 21:25:52 -0300

    Fixing pypi install/run problems due to classpaths

--- modules/kafman/src/main/kafman/__init__.py
+++ modules/kafman/src/main/kafman/__init__.py
@@ -1,13 +1,14 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-kafman v0.9.38
+# hspylib-kafman v0.9.44
 #
 # Package: main.kafman
 """Package initialization."""
 
 __all__ = [
+  '__classpath__', 
   '__main__', 
   'core', 
   'views'
 ]
-__version__ = '0.9.38'
+__version__ = '0.9.44'
