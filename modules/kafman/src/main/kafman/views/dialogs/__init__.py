f29568e4421245f9f49af899c6d83478249ece0b
Hugo Saporetti Junior <yorevs@gmail.com>
2022-06-30 22:49:47 -0300

    Minor fixes

--- modules/kafman/src/main/kafman/views/dialogs/__init__.py
+++ modules/kafman/src/main/kafman/views/dialogs/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-kafman v0.9.64
+# hspylib-kafman v0.9.65
 #
 # Package: main.kafman.views.dialogs
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
   'filters_dialog', 
   'settings_dialog'
 ]
-__version__ = '0.9.64'
+__version__ = '0.9.65'
