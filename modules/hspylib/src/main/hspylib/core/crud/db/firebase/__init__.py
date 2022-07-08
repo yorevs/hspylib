68eddb5fcd9469d2e8372b8e48c6e8e4466b638e
Hugo Saporetti Junior <yorevs@gmail.com>
2022-07-07 22:14:11 -0300

    Fix text_tools issues

--- modules/hspylib/src/main/hspylib/core/crud/db/firebase/__init__.py
+++ modules/hspylib/src/main/hspylib/core/crud/db/firebase/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v0.11.90
+# hspylib v0.11.91
 #
 # Package: main.hspylib.core.crud.db.firebase
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
   'firebase_config', 
   'firebase_repository'
 ]
-__version__ = '0.11.90'
+__version__ = '0.11.91'
