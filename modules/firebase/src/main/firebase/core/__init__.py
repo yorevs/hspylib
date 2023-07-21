b211fd9e5a1c0629aa4170c8f5798cc2f7c3740b
Hugo Saporetti Junior <yorevs@gmail.com>
2023-07-21 18:04:05 -0300

    Add input validation for firebase setup

--- modules/firebase/src/main/firebase/core/__init__.py
+++ modules/firebase/src/main/firebase/core/__init__.py
@@ -1,14 +1,14 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-firebase v0.9.108
+# hspylib-firebase v0.9.109
 #
 # Package: main.firebase.core
 """Package initialization."""
 
 __all__ = [
-  'agent_config', 
-  'file_processor', 
-  'firebase', 
-  'firebase_auth'
+    'agent_config', 
+    'file_processor', 
+    'firebase', 
+    'firebase_auth'
 ]
-__version__ = '0.9.108'
+__version__ = '0.9.109'
