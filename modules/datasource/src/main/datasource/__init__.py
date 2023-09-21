32cc842b6d65029385073a652463f7050ae0d3c9
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 17:42:09 +0000

    [@yorevs] New PyPi deployment. Module: datasource

--- modules/datasource/src/main/datasource/__init__.py
+++ modules/datasource/src/main/datasource/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-datasource v0.9.53
+# hspylib-datasource v0.9.55
 #
 # Package: main.datasource
 """Package initialization."""
@@ -18,4 +18,4 @@ __all__ = [
     'redis', 
     'sqlite'
 ]
-__version__ = '0.9.53'
+__version__ = '0.9.55'
--- modules/datasource/src/test/datasource/__init__.py
+++ modules/datasource/src/test/datasource/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-datasource v0.9.53
+# hspylib-datasource v0.9.55
 #
 # Package: test.datasource
 """Package initialization."""
@@ -11,4 +11,4 @@ __all__ = [
     'test_redis_repository', 
     'test_sqlite_repository'
 ]
-__version__ = '0.9.53'
+__version__ = '0.9.55'
