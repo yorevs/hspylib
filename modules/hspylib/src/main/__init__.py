cc3a524b3679b271bbe98ad6ea1c48a4379db745
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 21:01:31 +0000

    [@yorevs] New PyPi deployment. Module: hspylib

--- modules/hspylib/src/main/__init__.py
+++ modules/hspylib/src/main/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v1.12.18
+# hspylib v1.12.19
 #
 # Package: main
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
     'hspylib'
 ]
-__version__ = '1.12.18'
+__version__ = '1.12.19'
