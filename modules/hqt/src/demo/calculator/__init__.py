46348e779640943afd3e686dfbe611f8ec910170
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 21:00:16 +0000

    [@yorevs] New PyPi deployment. Module: hqt

--- modules/hqt/src/demo/calculator/__init__.py
+++ modules/hqt/src/demo/calculator/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-hqt v0.9.27
+# hspylib-hqt v0.9.28
 #
 # Package: demo.calculator
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
     'core', 
     'views'
 ]
-__version__ = '0.9.27'
+__version__ = '0.9.28'
--- modules/hqt/src/main/hqt/promotions/__init__.py
+++ modules/hqt/src/main/hqt/promotions/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-hqt v0.9.27
+# hspylib-hqt v0.9.28
 #
 # Package: main.hqt.promotions
 """Package initialization."""
@@ -16,4 +16,4 @@ __all__ = [
     'htableview', 
     'htoolbox'
 ]
-__version__ = '0.9.27'
+__version__ = '0.9.28'
