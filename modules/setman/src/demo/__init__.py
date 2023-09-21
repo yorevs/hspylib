5c0b22bac94d5dec125f58f9277b98b10aeb92ab
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 19:08:57 +0000

    [@yorevs] New PyPi deployment. Module: setman

--- modules/setman/src/demo/__init__.py
+++ modules/setman/src/demo/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-setman v0.10.21
+# hspylib-setman v0.10.22
 #
 # Package: demo
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
     'settings'
 ]
-__version__ = '0.10.21'
+__version__ = '0.10.22'
