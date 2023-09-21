b30b1b1fb81c12f869c7acb967a444f27eba2404
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 17:00:17 +0000

    [@yorevs] New PyPi publish: datasource

--- modules/datasource/src/main/datasource/exception/__init__.py
+++ modules/datasource/src/main/datasource/exception/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-datasource v0.9.44
+# hspylib-datasource v0.9.45
 #
 # Package: main.datasource.exception
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
     'exceptions'
 ]
-__version__ = '0.9.44'
+__version__ = '0.9.45'
