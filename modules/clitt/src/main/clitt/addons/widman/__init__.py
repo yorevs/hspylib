30ec6c8842895e557d464d5661628910fee35b1b
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 20:56:27 +0000

    [@yorevs] New PyPi deployment. Module: clitt

--- modules/clitt/src/main/clitt/addons/widman/__init__.py
+++ modules/clitt/src/main/clitt/addons/widman/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-clitt v0.9.95
+# hspylib-clitt v0.9.96
 #
 # Package: main.clitt.addons.widman
 """Package initialization."""
@@ -11,4 +11,4 @@ __all__ = [
     'widgets', 
     'widman'
 ]
-__version__ = '0.9.95'
+__version__ = '0.9.96'
