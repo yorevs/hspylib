c1abb9b9335a2c01def6d8e19875162ddd7f6f3e
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-09-21 17:42:09 +0000

    [@yorevs] New PyPi deployment. Module: datasource

--- modules/datasource/src/demo/phonebook/repository/__init__.py
+++ modules/datasource/src/demo/phonebook/repository/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-datasource v0.9.53
+# hspylib-datasource v0.9.55
 #
 # Package: demo.phonebook.repository
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
     'company_repository', 
     'person_repository'
 ]
-__version__ = '0.9.53'
+__version__ = '0.9.55'
