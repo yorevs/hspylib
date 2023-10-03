75d3cc0057a9ce909adde3272403020382802aeb
Hugo Saporetti Junior <yorevs@gmail.com>
2023-10-03 22:23:49 +0000

    [@yorevs] New PyPi deployment. Module: firebase

--- modules/firebase/src/main/__init__.py
+++ modules/firebase/src/main/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-firebase v0.9.135
+# hspylib-firebase v0.9.136
 #
 # Package: main
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
     'firebase'
 ]
-__version__ = '0.9.135'
+__version__ = '0.9.136'
