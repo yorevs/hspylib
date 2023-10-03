6ab6764bc4d9959ce612ff01559ca939d462bacf
Hugo Saporetti Junior <yorevs@gmail.com>
2023-10-03 22:19:16 +0000

    [@yorevs] New PyPi deployment. Module: cfman

--- modules/cfman/src/main/cfman/__init__.py
+++ modules/cfman/src/main/cfman/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-cfman v0.9.132
+# hspylib-cfman v0.9.133
 #
 # Package: main.cfman
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
     'core', 
     'exception'
 ]
-__version__ = '0.9.132'
+__version__ = '0.9.133'
