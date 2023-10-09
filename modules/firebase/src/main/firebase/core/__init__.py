d44e142b2bfe45b03943f6473c259a4795138d57
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 04:16:11 +0000

    [@yorevs] Push for new deployment -> 'firebase'

--- modules/firebase/src/main/firebase/core/__init__.py
+++ modules/firebase/src/main/firebase/core/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-firebase v0.9.136
+# hspylib-firebase v0.9.137
 #
 # Package: main.firebase.core
 """Package initialization."""
@@ -11,4 +11,4 @@ __all__ = [
     'firebase', 
     'firebase_auth'
 ]
-__version__ = '0.9.136'
+__version__ = '0.9.137'
