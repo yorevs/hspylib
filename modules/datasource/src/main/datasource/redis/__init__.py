ff0a8016969d802456b5710c3c832831ba6f982d
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 04:15:00 +0000

    [@yorevs] Push for new deployment -> 'datasource'

--- modules/datasource/src/main/datasource/redis/__init__.py
+++ modules/datasource/src/main/datasource/redis/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-datasource v0.9.67
+# hspylib-datasource v0.9.68
 #
 # Package: main.datasource.redis
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
     'redis_configuration', 
     'redis_repository'
 ]
-__version__ = '0.9.67'
+__version__ = '0.9.68'
