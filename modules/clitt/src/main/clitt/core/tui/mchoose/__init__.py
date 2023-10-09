5e7970633cb0ea4f3f4ef24e7bec62020fd2ef35
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 04:13:36 +0000

    [@yorevs] Push for new deployment -> 'clitt'

--- modules/clitt/src/main/clitt/core/tui/mchoose/__init__.py
+++ modules/clitt/src/main/clitt/core/tui/mchoose/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-clitt v0.9.98
+# hspylib-clitt v0.9.99
 #
 # Package: main.clitt.core.tui.mchoose
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
     'mchoose', 
     'menu_choose'
 ]
-__version__ = '0.9.98'
+__version__ = '0.9.99'
