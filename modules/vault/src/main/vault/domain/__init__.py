bcc19ca2ddfe322670ce38b74579f67a0a151007
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 04:22:27 +0000

    [@yorevs] Push for new deployment -> 'vault'

--- modules/vault/src/main/vault/domain/__init__.py
+++ modules/vault/src/main/vault/domain/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-vault v0.9.120
+# hspylib-vault v0.9.121
 #
 # Package: main.vault.domain
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
     'vault_entry'
 ]
-__version__ = '0.9.120'
+__version__ = '0.9.121'
