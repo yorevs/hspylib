1bf3fd391df91ceaf6e399da2eef2bd7a4694530
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 04:20:07 +0000

    [@yorevs] Push for new deployment -> 'kafman'

--- modules/kafman/src/test/__init__.py
+++ modules/kafman/src/test/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-kafman v0.9.135
+# hspylib-kafman v0.9.136
 #
 # Package: test
 """Package initialization."""
@@ -8,4 +8,4 @@
 __all__ = [
     'test_main'
 ]
-__version__ = '0.9.135'
+__version__ = '0.9.136'
