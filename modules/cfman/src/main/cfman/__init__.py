4ec3311929336d343eefe7005be41e1421db87ce
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 04:12:18 +0000

    [@yorevs] Push for new deployment -> 'cfman'

--- modules/cfman/src/main/cfman/__init__.py
+++ modules/cfman/src/main/cfman/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-cfman v0.9.133
+# hspylib-cfman v0.9.134
 #
 # Package: main.cfman
 """Package initialization."""
@@ -9,4 +9,4 @@ __all__ = [
     'core', 
     'exception'
 ]
-__version__ = '0.9.133'
+__version__ = '0.9.134'
