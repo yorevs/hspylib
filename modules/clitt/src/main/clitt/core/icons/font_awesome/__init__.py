9520a4d0869419e3bf15573aeb1f15c1644cd89a
Hugo Saporetti Junior <hugo_junior@gap.com>
2023-10-09 06:27:48 +0000

    [@yorevs] Push for new deployment -> ''

--- modules/clitt/src/main/clitt/core/icons/font_awesome/__init__.py
+++ modules/clitt/src/main/clitt/core/icons/font_awesome/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib-clitt v0.9.101
+# hspylib-clitt v0.9.102
 #
 # Package: main.clitt.core.icons.font_awesome
 """Package initialization."""
@@ -15,4 +15,4 @@ __all__ = [
     'trickplay_icons', 
     'widget_icons'
 ]
-__version__ = '0.9.101'
+__version__ = '0.9.102'
--- modules/hspylib/src/main/hspylib/modules/__init__.py
+++ modules/hspylib/src/main/hspylib/modules/__init__.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 #
-# hspylib v1.12.24
+# hspylib v1.12.25
 #
 # Package: main.hspylib.modules
 """Package initialization."""
@@ -13,4 +13,4 @@ __all__ = [
     'fetch', 
     'security'
 ]
-__version__ = '1.12.24'
+__version__ = '1.12.25'
